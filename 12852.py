9dd5a71b605cd391279bb217a49520288c0eac77
HyunkyuKim <hyunkyu001@gmail.com>
2021-04-06 15:12:38 +0900

    20210406

--- 12852.py
+++ 12852.py
@@ -12,15 +12,16 @@ for i in range(2, n+1):
         d[i] = min(d[i], d[i // 3] + 1)
 
 print(d[n])
+#########################
 
 while True:
-    print(int(n), end=" ")
+    print(n, end=" ")
     if n == 1:
         break
 
-    if n % 3 == 0 and d[int(n//3)] <= d[int(n-1)]:
-        n /= 3
-    elif n % 2 == 0 and d[int(n//2)] <= d[int(n-1)]:
-        n /= 2
+    if n % 3 == 0 and d[n // 3] < d[n-1]:
+        n //= 3
+    elif n % 2 == 0 and d[n // 2] < d[n-1]:
+        n //= 2
     else:
         n -= 1
