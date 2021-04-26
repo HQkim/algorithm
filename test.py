f1dee41222f1881f3273ec2fd919df7b5c2b35ed
Hyunkyu Kim <hyunkyu001@gmail.com>
2021-04-26 15:49:08 +0900

    1

--- test.py
+++ test.py
@@ -1,4 +1,39 @@
-a = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-n_2 = len(a)//2
+import sys
+from collections import deque
 
-print([row[:n_2] for row in a[:n_2]])
+input = sys.stdin.readline
+
+n, m, v = map(int, input().rstrip().split())
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().rstrip().split())
+    graph[a][b] = graph[b][a] = 1
+
+
+def dfs(v, visited):
+    visited[v] = 1
+    print(v, end=" ")
+    for i in range(1, n+1):
+        if visited[i] == 0 and graph[v][i] == 1:
+            dfs(i, visited)
+
+
+def bfs(v, visited):
+    queue = deque([v])
+    visited[v] = 1
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in range(1, n+1):
+            if visited[i] == 0 and graph[v][i] == 1:
+                queue.append(i)
+                visited[i] = 1
+
+
+visited_dfs = [0]*(n+1)
+visited_bfs = [0]*(n+1)
+
+dfs(v, visited_dfs)
+print()
+bfs(v, visited_bfs)
