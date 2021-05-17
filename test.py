f772803858de40e1a52a9c08015cbc0abce7b562
Hyunkyu Kim <hyunkyu001@gmail.com>
2021-05-17 11:46:28 +0900

    1문제

--- test.py
+++ test.py
@@ -1,27 +1,11 @@
 import sys
+M = 9**9
+sys.setrecursionlimit(M)
+a = [*map(int, sys.stdin.read().split()), M]
 
-input = sys.stdin.readline
 
-n =int(input())
-arry = list(map(int, input().rstrip().split()))
+def f(i, u): p = a[i]; i += 1; i += p > a[i] and f(i, min(u, p)
+                                                   )-i; i += u > a[i] and f(i, u)-i; print(p); return i
 
-nge = [-1]*n
-stack = []
 
-for i in range(n-2, -1, -1):
-    right = arry[i+1]
-    stack.append(right)
-
-    if arry[i] < right:
-        nge[i] = right
-        continue
-    
-    while stack:
-        num = stack.pop()
-        if arry[i] < num:
-            nge[i] = num
-            stack.append(num)
-            break
-
-print(*nge)
-                
+f(0, M)
