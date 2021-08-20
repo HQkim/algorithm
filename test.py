0c3fb044cb0c8ede4d508f9beea4c431d7f0720b
Hyunkyu Kim <hyunkyu001@gmail.com>
2021-08-20 14:04:33 +0900

    test파일 수정

--- test.py
+++ test.py
@@ -1,40 +1,26 @@
-def DFS(v):
+def dfs(v):
     global ans
-    if v==99:
+    if v == 99:
         ans = 1
         return
 
-    visited[v] = 1 
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
 
-    for w in adj_list[v]:
-        if not visited[w]:
-            DFS(w)
 
-for _ in range(10):
-    tc, N = list(map(int, input().split()))
-    road =  list(map(int, input().split))
-
-    adj_list = [[] for _ in range(100)]
-    for i in range(N):
-        adj_list[road[2 * i]].append(road[2* i + 1])
-
+T = 10
+for tc in range(1, T+1):
+    tc, n = map(int, input().split())
+    arr = list(map(int, input().split()))
+    graph = [[] for _ in range(100)]
     visited = [0] * 100
-    ans = 0 
 
-    stack = [0]
+    for i in range(n):
+        a, b = arr[i*2: (i+1)*2]
+        graph[a].append(b)
 
-    while stack:
-        curr = stack.pop()
+    ans = 0
+    dfs(0)
 
-        if curr == 90:
-            ans1 = 1
-            break
-
-        if visited[curr]: continue
-        visited[curr] = 1
-
-        for w in adj_list[curr]:
-            if not visited[w]:
-                stack.append(w)
-
-    print('#{} {}'.format(tc, ans))
+    print(f'#{tc} {ans}')
