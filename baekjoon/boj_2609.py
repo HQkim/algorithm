0e79547ffd9a457c6fa1720462e8fe0d40f1ee9b
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-07-25 19:41:10 +0900

    Update: boj 2609

--- baekjoon/boj_2609.py
+++ baekjoon/boj_2609.py
@@ -1,11 +1,15 @@
+# boj 2609 최대공약수와 최소공배수
+
+# 유클리드 호제법을 활용한 풀이
+def get_gcd(x, y):
+    while y > 0:
+        x, y = y, x % y
+    return x
+
 a, b = map(int, input().rstrip().split())
 
-max_div = 1
-for i in range(2, min(a, b)+1):
-    if a % i == 0 and b % i == 0:
-        max_div = i
+gcd = get_gcd(a, b)
+lcm = a * b //gcd
 
-min_multi = max_div * (a // max_div) * (b // max_div)
-
-print(max_div)
-print(min_multi)
+print(gcd)
+print(lcm)    
