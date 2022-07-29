3fe434d571c23077c9cce69a67d32662da43d7c7
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-07-29 12:23:06 +0900

    Create: [Pro] 2018카카오블라인드 [3차]압축

---
+++ programmers/2018카카오/[3차]압축.py
@@ -0,0 +1,32 @@
+def solution(msg):
+    answer = []
+    d = {}
+    for i in range(65, 91):
+        d[chr(i)] = i - 64
+
+    num = 27
+    w = msg[0]
+    while msg:
+        msg = msg[1:]
+        if msg:
+            c = msg[0]
+        else:
+            c = ''
+            
+        if w + c in d:
+            w = w + c
+        else:
+            answer.append(d[w])
+            d[w+c] = num
+            num += 1
+            w = c
+
+    if w:
+        answer.append(d[w])
+
+    return answer
+
+
+solution("KAKAO")
+solution("TOBEORNOTTOBEORTOBEORNOT")
+solution("ABABABABABABABAB")
