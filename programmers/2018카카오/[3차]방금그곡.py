1bfcee1f44adb66ace9a0fbb6488de33e99d51d4
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-07-29 11:37:29 +0900

    Create: [Pro] 2018카카오블라인드 [3차]방금그곡

---
+++ programmers/2018카카오/[3차]방금그곡.py
@@ -0,0 +1,39 @@
+# programmers 2018카카오블라인드 [3차]방금그곡
+
+def solution(m, musicinfos):
+    answer = ''
+    candidate = []
+    d_r = {'C#': 'c', 'D#': 'd', 'F#': 'f', 'G#': 'g', 'A#': 'a'}
+    for k,v in d_r.items():
+        if k in m:
+            m = m.replace(k, v)
+
+    for music in musicinfos:
+        start, end, title, melody = music.split(",")
+        for k,v in d_r.items():
+            if k in melody:
+                melody = melody.replace(k, v)
+        time = convert_time(end) - convert_time(start)
+        melody_length = len(melody)
+        over_cycle= time // melody_length
+        over_res = time % melody_length
+        real_melody = melody*over_cycle + melody[:over_res]
+        if m in real_melody:
+            candidate.append((title, time))
+    
+    if not candidate:
+        return "(None)"
+
+    candidate = list(enumerate(candidate))
+    candidate.sort(key=lambda x: (-x[1][1], x[0]))
+    answer = candidate[0][1][0]
+
+    return answer
+
+def convert_time(time):
+    h, m = time.split(":")
+    result = int(h)*60 + int(m)
+    return result
+
+solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"])
+solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"])
