c690ee58dfd52467d38970307da4441bf16c0193
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-08-10 11:07:04 +0900

    Create: [BOJ] 11726 2xn 타일링

---
+++ baekjoon/boj_11726.py
@@ -0,0 +1,14 @@
+# BOJ 11726 2xn 타일링
+
+n = int(input())
+
+arr = [0]*(n+1)
+if n > 0:
+    arr[1] = 1
+if n > 1:
+    arr[2] = 2 
+
+for i in range(3, n+1):
+    arr[i] = arr[i-1] + arr[i-2]
+
+print(arr[n]%10007)
