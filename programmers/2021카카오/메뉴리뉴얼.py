289010b09bc35039929b4c8332e69ed485c7ba30
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-08-22 14:39:44 +0900

    Update: [Pro] 2021카카오 메뉴리뉴얼

--- programmers/2021카카오/메뉴리뉴얼.py
+++ programmers/2021카카오/메뉴리뉴얼.py
@@ -19,7 +19,8 @@ def solution(orders, course):
 
         if d:
             if max(d.values()) >= 2:
-                answer += [k for k, v in d.items() if max(d.values()) == v]
+                max_v = max(d.values())
+                answer += [k for k, v in d.items() if v == max_v]
 
     answer.sort()
     return answer
