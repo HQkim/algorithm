356ea4d39b491fea31346b1ced8d60fbc6bfa379
Hyunkyu Kim <hyunkyu001@gmail.com>
2022-08-24 12:11:30 +0900

    Update: [BOJ 1197] 최소 스패닝 트리
    프림 알고리즘 추가

--- baekjoon/boj_1197.py
+++ baekjoon/boj_1197.py
@@ -2,6 +2,7 @@
 
 import sys
 
+#### 크루스칼 알고리즘 ####
 # 부모를 찾는 함수
 def find_parent(x):
     if parent[x] != x:
@@ -39,3 +40,32 @@ for edge in edges:
         total += c
     
 print(total)
+
+
+#### 프림 알고리즘 ####
+INF = int(10e9)
+V, E = map(int, input().split())
+graph = [[] for _ in range(V + 1)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+visited = [0] * (V + 1)
+distance = [INF] * (V + 1)
+distance[0], distance[1] = 0, 0
+
+for i in range(V):
+    min_dist = INF
+    for j in range(1, V + 1):
+        if not visited[j] and distance[j] <= min_dist:
+            now = j
+            min_dist = distance[j]
+
+    visited[now] = 1
+
+    for v, cost in graph[now]:
+        if not visited[v] and cost < distance[v]:
+            distance[v] = cost
+
+print(sum(distance))
